59701a3dc72690cf7a5506c5980ac29e39977f2b
filipebezerra95 <filipebza2019@gmail.com>
2025-12-23 22:45:02 -0300

    first commit

---
+++ numbersChoise.js
@@ -0,0 +1,82 @@
+const gameChoice = [
+
+    {group: 'group1', game: 'A1', numbers: [3, 6, 10, 24, 51, 53]},
+    {group: 'group1', game: 'b1', numbers: [1, 11, 27, 40, 46, 58]},
+    {group: 'group1', game: 'c1', numbers: [2, 10, 34, 36, 45, 55]},
+
+    {group: 'group1', game: 'A2', numbers: [7, 18, 22, 36, 48, 55]},
+    {group: 'group1', game: 'b2', numbers: [7, 18, 22, 36, 48, 55]},
+    {group: 'group1', game: 'c2', numbers: [8, 12, 19, 21, 29, 32]},
+
+    {group: 'group1', game: 'A3', numbers: [5, 16, 25, 33, 47, 55]},
+    {group: 'group1', game: 'b3', numbers: [2, 7, 11, 24, 39, 53]},
+    {group: 'group1', game: 'c3', numbers: [3, 10, 25, 29, 48, 52]},
+
+    {group: 'group1', game: 'A4', numbers: [3, 9, 23, 36, 41, 49]},
+    {group: 'group1', game: 'b4', numbers: [1, 15, 29, 31, 33, 47]},
+    {group: 'group1', game: 'c4', numbers: [6, 20, 27, 34, 45, 51]},
+
+    //----------------- GROUP 2 -----------------//
+
+    {group: 'group2', game: 'A1', numbers: [5, 16, 22, 25, 32, 47]},
+    {group: 'group2', game: 'b1', numbers: [6, 19, 27, 35, 48, 51]},
+    {group: 'group2', game: 'c1', numbers: [6, 21, 25, 32, 47, 59]},
+
+    {group: 'group2', game: 'A2', numbers: [2, 6, 11, 26, 48, 50]},
+    {group: 'group2', game: 'b2', numbers: [3, 8, 19, 37, 41, 45]},
+    {group: 'group2', game: 'c2', numbers: [4, 12, 26, 40, 42, 48]},
+
+    {group: 'group2', game: 'A3', numbers: [2, 9, 34, 35, 46, 56]},
+    {group: 'group2', game: 'b3', numbers: [1, 5, 22, 24, 51, 53]},
+    {group: 'group2', game: 'c3', numbers: [1, 7, 22, 51, 55, 59]},
+
+    {group: 'group2', game: 'A4', numbers: [8, 16, 18, 25, 37, 48]},
+    {group: 'group2', game: 'b4', numbers: [9, 16, 22, 38, 42, 58]},
+    {group: 'group2', game: 'c4', numbers: [9, 12, 22, 36, 38, 42]},
+
+    //----------------- GROUP 3 -----------------//
+
+    {group: 'group3', game: 'A1', numbers: [4, 18, 26, 35, 41, 60]},
+    {group: 'group3', game: 'b1', numbers: [10, 12, 27, 34, 45, 58]},
+    {group: 'group3', game: 'c1', numbers: [7, 12, 19, 33, 42, 56]},
+
+    {group: 'group3', game: 'A2', numbers: [8, 15, 18, 33, 40, 50]},
+    {group: 'group3', game: 'b2', numbers: [3, 7, 12, 40, 44, 56]},
+    {group: 'group3', game: 'c2', numbers: [4, 7, 10, 12, 33, 58]},
+
+    {group: 'group3', game: 'A3', numbers: [5, 19, 22, 24, 32, 42]},
+    {group: 'group3', game: 'b3', numbers: [5, 18, 21, 24, 33, 43]},
+    {group: 'group3', game: 'c3', numbers: [4, 13, 26, 36, 40, 48]},
+
+    {group: 'group3', game: 'A4', numbers: [5, 13, 20, 27, 42, 56]},
+    {group: 'group3', game: 'b4', numbers: [6, 23, 34, 35, 47, 52]},
+    {group: 'group3', game: 'c4', numbers: [7, 11, 31, 35, 58, 60]},
+
+    //----------------- GROUP 4 -----------------//
+
+    {group: 'group4', game: 'A1', numbers: [4, 12, 19, 25, 26, 56]},
+    {group: 'group4', game: 'b1', numbers: [3, 18, 23, 40, 46, 54]},
+    {group: 'group4', game: 'c1', numbers: [2, 11, 19, 21, 25, 36]},
+
+    {group: 'group4', game: 'A2', numbers: [8, 17, 25, 53, 55, 60]},
+    {group: 'group4', game: 'b2', numbers: [13, 16, 36, 43, 48, 55]},
+    {group: 'group4', game: 'c2', numbers: [3, 21, 22, 36, 44, 55]},
+
+    {group: 'group4', game: 'A3', numbers: [2, 24, 27, 32, 48, 57]},
+    {group: 'group4', game: 'b3', numbers: [4, 12, 15, 29, 40, 45]},
+    {group: 'group4', game: 'c3', numbers: [3, 9, 10, 12, 37, 41]},
+
+    {group: 'group4', game: 'A4', numbers: [9, 12, 23, 36, 42, 50]},
+    {group: 'group4', game: 'b4', numbers: [4, 8, 11, 13, 31, 56]},
+    {group: 'group4', game: 'c4', numbers: [3, 5, 11, 18, 33, 47]},
+
+     //----------------- GROUP 4 -----------------//
+
+    {group: 'group5', game: 'A1', numbers: [15, 18, 33, 38, 54, 58]},
+    {group: 'group5', game: 'b1', numbers: [24, 28, 35, 40, 47, 59]},
+    {group: 'group5', game: 'c1', numbers: [7, 22, 36, 38, 42, 56]},
+
+    {group: 'group5', game: 'A2', numbers: [3, 22, 23, 36, 44, 55]},
+
+        
+]
